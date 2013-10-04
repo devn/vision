c7e7e36e33f085ed437156662e6b015d36d84660
Devin Walters <devinw@gmail.com>
2013-10-04 16:49:37 -0500

    Bump clojure and incubator versions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject vision "1.0.0-SNAPSHOT"
   :description "OpenCV wrapper for Clojure."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.incubator "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.incubator "0.1.3"]
                  [org.clojars.nakkaya/jna "3.2.7"]]
   :jvm-opts ["-Djna.library.path=resources/lib/"])
