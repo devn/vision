7a9da17f2566a7f2c0e7055daa8ce30579b24359
Devin Walters <devinw@gmail.com>
2013-10-04 16:57:17 -0500

    Add URL and namespace to my clojars username

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject vision "1.0.0-SNAPSHOT"
+(defproject devn/vision "1.0.0-SNAPSHOT"
   :description "OpenCV wrapper for Clojure."
+  :url "https://github.com/devn/vision"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojars.nakkaya/jna "3.2.7"]]
